78faffc041f8de0ed9877c7eddc727200d6a693e
ka <ka.kaosf@gmail.com>
2015-08-19 16:50:26 +0900

    Run "rails g model following from_user:references to_user:references"

---
+++ app/models/following.rb
@@ -0,0 +1,4 @@
+class Following < ActiveRecord::Base
+  belongs_to :from_user
+  belongs_to :to_user
+end
---
+++ spec/factories/followings.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :following do
+    from_user nil
+to_user nil
+  end
+
+end

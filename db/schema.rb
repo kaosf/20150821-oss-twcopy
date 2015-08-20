a5a02e823da2603563b2b8bb855f85d09bd9f743
ka <ka.kaosf@gmail.com>
2015-08-20 21:21:20 +0900

    Run "rake db:migrate"

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150819074944) do
+ActiveRecord::Schema.define(version: 20150820121930) do
 
   create_table "followings", force: :cascade do |t|
     t.integer  "from_user_id", null: false
@@ -46,6 +46,10 @@ ActiveRecord::Schema.define(version: 20150819074944) do
     t.string   "name",                   default: "", null: false
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "icon_file_name"
+    t.string   "icon_content_type"
+    t.integer  "icon_file_size"
+    t.datetime "icon_updated_at"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true

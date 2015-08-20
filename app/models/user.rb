0df5b2b93aa47d2582bf4ccccee9cf974f3b1aab
ka <ka.kaosf@gmail.com>
2015-08-20 21:24:24 +0900

    Enable to upload user icon image

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,4 +10,7 @@ class User < ActiveRecord::Base
   has_many :followers, foreign_key: :to_user_id, class_name: 'Following', dependent: :destroy
   has_many :following_users, through: :followings, source: :to_user
   has_many :follower_users, through: :followers, source: :from_user
+
+  has_attached_file :icon
+  validates_attachment_content_type :icon, content_type: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif']
 end
